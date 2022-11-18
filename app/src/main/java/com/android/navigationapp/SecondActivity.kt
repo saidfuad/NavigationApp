851548ad55cad17e683478be4dd4bcab0122492c
Said Fuad <saidfuad91@gmail.com>
2022-11-18 15:37:55 +0300

    Added SecondActivity.kt

--- app/src/main/java/com/android/navigationapp/SecondActivity.kt
+++ app/src/main/java/com/android/navigationapp/SecondActivity.kt
@@ -8,17 +8,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import com.android.navigationapp.ui.theme.NavigationAppTheme
 
-class MainActivity : ComponentActivity() {
+class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -28,13 +21,8 @@ class MainActivity : ComponentActivity() {
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ){
-            
-                Button(onClick = { 
-                    val navigate = Intent(this@MainActivity, SecondActivity::class.java)
-                    startActivity(navigate)
-                }) {
-                    Text(text = "Navigate", fontSize = 18.sp)
-                }
+
+
             }
 
 
